7eda5b1f2414706b599e826a25a1796b0bb50c84
Nils Riedemann <nils@202-Nils.local>
2009-08-03 16:23:33 +0200

    initial commit

---
+++ README.markdown
@@ -0,0 +1,54 @@
+# really Simple Tooltips
+
+A jQuery Plugin for really simple tooltips.
+
+## Installation and Usage
+
+### Installation
+
+When you're reading this, i bet you are familiar with how to install a jquery Plugin. Well, just load the jquery.tooltip.js after the man jQuery file and you're done.
+
+### Usage
+
+To activate Tooltips just do the following:
+
+    $("a[title]").tooltip();
+
+This will activate Tooltips for all Links, that have a title set. The Plugin will display the value of the title attribute within the Tooltip.
+
+### Options
+
+You can define following options
+
+- offset: The offset of tooltip relative to the cursors position (default 15)
+- offset_x: The x-offset of the tooltip, relative to the cursors position
+- offset_y: The x-offset of the tooltip, releative to the cursors position 
+- attribute: The attribute of the hovered element that has the text for the tooltip (default: title)
+
+#### Example
+
+    $("a[title]").tooltip({
+    	attribute: "longdesc",
+	offset: 30
+    });
+
+### Customization
+
+Simple Tooltip is meant to be really simple. Just use this basic CSS and modify it to your needs:
+
+
+    #tooltip-margin {
+    	width: 150px;
+    	background-color: rgb(255,255,255);
+    	background: rgba(255,255,255,0.9);
+    	z-index: 31337;
+    	
+    	-webkit-border-radius: 5px;
+    	-moz-border-radius: 5px;
+    	-webkit-box-shadow: 0px 5px 10px rgba(0,0,0,0.7);
+    }
+    
+    #tooltip {
+    	padding: 5px;
+    	color: #000;
+    }
---
+++ jquery.tooltip.js
@@ -0,0 +1,68 @@
+/*
+ * jQuery really Simple Tooltips
+ * version: 0.5
+ * @requires jQuery v1.3.2 or later
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ *
+ * "Official" repository: http://github.com/nilsr/really-simple-tooltips
+ * @author Nils Riedemann < nils.r at gmx dot com> http://beatsundbytes.de
+ */
+
+(function($){
+	$.fn.tooltip = function(options) {
+		var defaults = {
+			classname: "tooltip",
+			offset: 15,
+			offset_x: 0,
+			offset_y: 0,
+			attribute: "title"
+		};
+			
+		var options = $.extend(defaults,options);
+		
+		return this.each(function(){
+			element = $(this);
+			
+			// Offsets festlegen
+			if(options['offset_x'] == 0){ options['offset_x'] = options['offset']; }
+			if(options['offset_y'] == 0){ options['offset_y'] = options['offset']; }
+			
+			element.hover(function(e){
+
+				if($("#tooltip-margin").length) {
+					// Tootip already present
+				} else {
+					this.t = $(this).attr(options["attribute"]);
+					
+					$(this).attr("title",null);
+					
+					// Tooltip erstellen
+					$("body").append("<div id=\"tooltip-margin\"><div id=\"tooltip\">"+this.t+"</div></div>");
+					
+					// Position festlegen
+					var top_position = (e.pageY + options['offset_y']);
+					var left_position = (e.pageX + options['offset_x']);
+					
+					// Nach Links ausrichten wenn's zu groß werden würde
+					if ((e.pageX+options['offset_x']+$("#tooltip-margin").width()) > $("body").width()) {
+						left_position = e.pageX - options['offset_x'] - $("#tooltip-margin").width();
+					}
+					
+					// Positionieren
+					$("#tooltip-margin").css({
+						"position":"absolute",
+						"top": top_position+"px",
+						"left": left_position+"px"
+					});
+				}
+				
+			}, function() {
+				this.title = this.t
+				$("#tooltip-margin").remove();
+			})
+		});
+	}
+})(jQuery)
